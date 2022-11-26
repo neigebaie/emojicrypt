a3f30ee93e45b034c8477e1c5ae8b22f24fa6da9
neigebaie <paul.serander@gmail.com>
2022-11-26 22:13:40 +0100

    added verification for charsets

--- emojicrypt.py
+++ emojicrypt.py
@@ -1,5 +1,18 @@
-alpha_1="abcdefghijklmnopqrstuvwxyz'!? "
+alpha_1="abcdefghijklmnopqrstuvwxyz'!?à "
 alpha_2=["🐒", "🦍", "🐶", "🐺", "🦊", "🦟", "🦝", "🐱", "🦁", "🐯", "🐴", "🦄", "🦌", "🦬", "🐮", "🐖", "🐐", "🐪", "🦒", "🐘", "🦏", "🦣", "🐀", "🦦", "🐼", "🦫", "🐇", "🦆", "🐋", "🐬", "🦭"]
+
+def test_alpha():
+	if len(alpha_1) != len(alpha_2):
+		raise Exception("Longueurs des deux alphabets différentes ! (%d != %d)" % (len(alpha_1), len(alpha_2)))
+	for char in alpha_1:
+		if alpha_1.count(char) > 1:
+			raise Exception("DUPPLICATE %s" % char)
+	for char in alpha_2:
+		if alpha_2.count(char) > 1:
+			raise Exception("DUPPLICATE %s" % char)
+
+test_alpha() # stops importation if errors found
+
 
 def encrypt(msg, a, b):
 	enc_msg = []
@@ -9,7 +22,7 @@ def encrypt(msg, a, b):
 		try:
 			c = (a * alpha_1.index(m) + (b + i)) % len(alpha_1)
 			enc_msg.append(alpha_2[c])
-			i += 1
+			# i += 1
 		except:
 			print("caractère \"" + m + "\" non trouvé !")
 
@@ -28,6 +41,6 @@ def decrypt(msg, a, b):
 	for c in msg:
 		m = a_1 * (alpha_2.index(c) - (b + i)) % len(alpha_1)
 		clear_msg.append(alpha_1[m])
-		i += 1
+		# i += 1
 
 	return "".join(clear_msg)
