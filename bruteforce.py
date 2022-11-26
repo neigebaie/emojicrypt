5cafea560e5d6d11360506fb5231a4c42f7a0aad
neigebaie <paul.serander@gmail.com>
2022-11-26 21:43:37 +0100

    added bruteforce script

---
+++ bruteforce.py
@@ -0,0 +1,42 @@
+import emojicrypt as ec
+
+def score(msg):
+    try:
+        with open('fr_dic.txt', 'r') as f:
+            wordlist = f.readlines()
+            wordlist = [word.strip('\n') for word in wordlist]
+        
+        score = 0
+
+        for word in msg.split(' '):
+            if word in wordlist:
+                score += len(word)
+        
+        return score
+    except:
+        print('Erreur : Le score n\'a pas pu être établi.')
+
+print('-- Bruteforce Emojicrypt --\n')
+
+enc_msg = input('message encrypté :')
+max_a = int(input('max_a = '))
+max_b = int(input('max_b = '))
+
+attempts = []
+
+print('\nbruteforcing in progress', end='')
+for a in range(max_a):
+    for b in range(max_b):
+        print('.', end='', flush=True)
+        attempt = {'msg': ec.decrypt(enc_msg, a, b), 'a': a, 'b': b}
+        attempt['score'] = score(attempt['msg'])
+        attempts.append(attempt)
+
+attempts.sort(key=lambda attempt: attempt['score'])
+
+res = attempts[-1:][0]
+
+if res['score'] > 5:
+    print('\n')
+    print('message : ' + res['msg'])
+    print('clé : (a = %d; b = %d)' % (res['a'], res['b']))
---
+++ emojicrypt.py
@@ -0,0 +1,33 @@
+alpha_1="abcdefghijklmnopqrstuvwxyz'!? "
+alpha_2=["🐒", "🦍", "🐶", "🐺", "🦊", "🦟", "🦝", "🐱", "🦁", "🐯", "🐴", "🦄", "🦌", "🦬", "🐮", "🐖", "🐐", "🐪", "🦒", "🐘", "🦏", "🦣", "🐀", "🦦", "🐼", "🦫", "🐇", "🦆", "🐋", "🐬", "🦭"]
+
+def encrypt(msg, a, b):
+	enc_msg = []
+	i = 0
+
+	for m in msg.lower():
+		try:
+			c = (a * alpha_1.index(m) + (b + i)) % len(alpha_1)
+			enc_msg.append(alpha_2[c])
+			i += 1
+		except:
+			print("caractère \"" + m + "\" non trouvé !")
+
+	return "".join(enc_msg)
+
+
+def decrypt(msg, a, b):
+	clear_msg = []
+	i = 0
+	a_1 = 0
+
+	for a_1 in range(len(alpha_1)):
+		if (a_1 * a) % len(alpha_1) == 1:
+			break
+
+	for c in msg:
+		m = a_1 * (alpha_2.index(c) - (b + i)) % len(alpha_1)
+		clear_msg.append(alpha_1[m])
+		i += 1
+
+	return "".join(clear_msg)
